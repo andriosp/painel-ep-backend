1968ae0a69c9ebf54e0a6ed3770d29c36aeff328
Andrio Pinto <andriopinto@MacBook-Air-de-Andrio.local>
2026-05-18 08:35:56 -0300

    Libera CORS para dominio Netlify correto

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://painel-ep.netlify.app",
+        "https://ofertassenairs.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
